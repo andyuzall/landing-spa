537e1e9d1cd08ccaa92cb3c2ea88c5cab6029644
Andy Uzal <andyuzal@gmail.com>
2024-09-23 04:47:01 -0300

    js-css-base

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function desplegarMenus() {
+    let a = document.getElementById('menuA')
+    let b = document.getElementsByClassName('menuB')
+    let c = document.getElementsByClassName('menuC')
+    
+    
+
+}
