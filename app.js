33d64a04303ba4092131b3b0fe17ba425c5a3597
Keibol <jjendo@protonmail.com>
2024-09-23 10:52:32 +0200

    final

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ function agregarTexto(event, index) {
     let contenedorTexto = document.getElementById('contenedor-texto')
     let botonPulsado = event.target;
     let valorBoton = event.target.value;
-    let texto = 'clickeado'
-    let texto2 = 'Segundo boton clickeado'
-    let texto3 = 'Tercer boton clickeado'
+    let texto = 'Lorem fistrum hasta luego Lucas llevame al sircoo tiene musho peligro quietooor no te digo trigo por no llamarte Rodrigor.'
+    let texto2 = 'Pecador diodeno amatomaa ahorarr a peich pupita. Se calle ustée al ataquerl amatomaa amatomaa por la gloria de mi madre por la gloria de mi madre. '
+    let texto3 = 'Jarl qué dise usteer ahorarr de la pradera está la cosa muy malar hasta luego Lucas a wan a gramenawer diodenoo qué dise usteer va usté muy cargadoo.'
     console.log(valorBoton)
     
     if (botonPulsado && valorBoton==='Inicio') {
@@ -29,7 +29,5 @@ function agregarTexto(event, index) {
     if(botonPulsado && valorBoton === 'Contacto'){
         contenedorTexto.innerHTML = texto3;
     }
-
-
 
 }
