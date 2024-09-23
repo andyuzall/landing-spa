573c9d34cbc6657250dd9c247b16c0b730c49cf6
Andy Uzal <andyuzal@gmail.com>
2024-09-23 05:37:17 -0300

    js-final

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 
 function desplegarMenus() {
-    let a = document.getElementById('menuA')
-    let b = document.getElementsByClassName('menuB')
-    let c = document.getElementsByClassName('menuC')
+    const botones = [...document.querySelectorAll('.botones')]
     
+    iniciar();
+
+    function iniciar() {
+        botones.forEach((boton, index) =>  {
+            boton.addEventListener('click', (event) => agregarTexto(event, index));
+        })
+    }
+}
+
+function agregarTexto(event, index) {
+    let contenedorTexto = document.getElementById('contenedor-texto')
+    let botonPulsado = event.target;
+    let valorBoton = event.target.value;
+    let texto = 'clickeado'
+    let texto2 = 'Segundo boton clickeado'
+    let texto3 = 'Tercer boton clickeado'
+    console.log(valorBoton)
     
+    if (botonPulsado && valorBoton==='Inicio') {
+        contenedorTexto.innerHTML = texto;
+    }
+    if(botonPulsado && valorBoton === 'Servicios'){
+        contenedorTexto.innerHTML = texto2;
+    }
+    if(botonPulsado && valorBoton === 'Contacto'){
+        contenedorTexto.innerHTML = texto3;
+    }
+
+
 
 }
